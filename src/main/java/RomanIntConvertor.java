29574ab460d1cdfd111b7acad479c2236c9399eb
Pascal Flores <pascal.flores@etu.univ-amu.fr>
2022-12-06 14:13:05 +0100

    romantoint returns 0 when roman number is empty

--- src/main/java/RomanIntConvertor.java
+++ src/main/java/RomanIntConvertor.java
@@ -1,2 +1,9 @@
 public class RomanIntConvertor {
+
+    public int RomanToInt(String romanNumber) {
+        if (romanNumber.isEmpty()) {
+            return 0;
+        }
+        return -1;
+    }
 }
--- src/test/java/RomanIntConvertorTest.java
+++ src/test/java/RomanIntConvertorTest.java
@@ -1,5 +1,13 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RomanIntConvertorTest {
-
+    
+    @Test
+    void shouldReturn0WhenEmpty() {
+        RomanIntConvertor romanIntConvertor = new RomanIntConvertor();
+        int result = romanIntConvertor.RomanToInt("");
+        assertEquals(0, result);
+    }
 }
